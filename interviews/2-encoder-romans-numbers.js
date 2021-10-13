392aec9fab48e05241e5ce9dba0632b9ee5d2ecc
Edgar HiSo <edghiso@gmail.com>
2021-10-13 17:15:50 +0200

    decoder romans numbers

---
+++ interviews/2-encoder-romans-numbers.js
@@ -0,0 +1,20 @@
+let number = 345
+function getRomansNumbers(number) {
+    const map = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90,L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+    let output = ""
+    for (let i in map) {
+        while (number >= map[i]) {
+            output += i;
+            number -= map[i]
+        }
+    }
+    return output
+}
+console.log(getRomansNumbers(number))
+
+let roman = "XVIII";
+function getNumber(roman) {
+    const map = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90,L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+    const number = 0;
+    
+}

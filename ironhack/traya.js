a515bbb625b103ce486db144344823ac902d0d72
Edgar HiSo <edghiso@gmail.com>
2021-10-21 19:05:54 +0200

    trying kata nº 59

--- ironhack/traya.js
+++ ironhack/traya.js
@@ -1,20 +1,26 @@
-function sortWord(str) {
-    return str.split("").sort().join("")
-}
-function isAnagram(str1, str2) {
-    return sortWord(str1) === sortWord(str2)
-}
-let array = ["dell", "ledl", "abc", "cba", "abc", "cba"]
+let arrayNames = [["Pablo", "Enrique", "Josh", "Juan", "Gonzalo"], ["Michael", "Alexander", "Mikel", "Ariel"], ["John", "Julia", "Brad", "George"]]
 
-function countAnagrams(array) {
-    let count = 0;
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-            if (isAnagram(array[i], array[j])) {
-                count++
-            }
+function flattenArray(arrayNames) {
+    let newArray = [];
+    for (let subarray of arrayNames) {
+        for (let elm of subarray) {
+            newArray.push(elm)
         }
     }
-    return count
+    return newArray
 }
-console.log(countAnagrams(array))
+
+function sortArray(arrayNames) {
+    let arrNamesNew = flattenArray(arrayNames)
+    return arrNamesNew.sort((name1, name2) => {
+        if (name1.lenght > name2.lenght) {
+            return 1
+        } else if (name2.lenght > name1.lenght) {
+            return -1
+        } else {
+            return name1.toString().localeCompare(name2.toString())
+        }
+    })
+}
+
+console.log(sortArray(arrayNames))

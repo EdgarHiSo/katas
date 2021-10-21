ae20d52388b25d7a8776286fddca96f666f0d086
Edgar HiSo <edghiso@gmail.com>
2021-10-21 18:42:58 +0200

    trying kata nº 56

--- ironhack/traya.js
+++ ironhack/traya.js
@@ -1,55 +1,52 @@
-// valid parens??
-function validparens(parens) {
-    map = { ")": "(", "}": "{", "]": "[" }
+function validParens(parens) {
+    let map = { ")": "(", "}": "{", "]": "[" }
     let stack = []
-    for (let char of parens) {
-        if (!map[char]) {
-            stack.push(char)
+    for (let paren of parens) {
+        if (!map[paren]) {
+            stack.push(paren)
         }
-        else if (stack.pop() !== map[char]) {
+        else if (stack.pop() !== map[paren]) {
             return false
         }
     }
     return stack.length == 0
 }
-console.log(validparens("(()))"))
 
-//coder numberRoman
-
-
-function getNumberFromRoman(number) {
-    let map = { M: 1000, CM: 900, D: 500, XD: 400, C: 100, XC: 900, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+function decoderRoman(number) {
+    let map = { 
+        M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90,
+        L: 50, XL: 40, X: 100, IX: 9, V: 5, IV: 4, I: 1 }
     let output = ""
-    for (let i in map)
+    for (let i in map) {
         while (number >= map[i]) {
-            output += i;
             number -= map[i]
+            output += i
         }
+    }
     return output
 }
-console.log(getNumberFromRoman(890))
+console.log(decoderRoman(95))
 
-function decoreded(roman) {
-    let map = { M: 1000, CM: 900, D: 500, XD: 400, C: 100, XC: 900, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+function fromRomanToNumber(roman) {
+    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 100, IX: 9, V: 5, IV: 4, I: 1 }
     let chars = roman.split("")
-    let number = 0
-    let i = 0
+    let count = 0
+    let i = 0;
     while (i < chars.length) {
         let char = chars[i]
         if (i < chars.length - 1) {
-            let next = chars[i + 1]
+            let next = chars[i + 1] 
             if (char + next in map) {
-                number += map[char + next]
+                count += map[char + next];
                 i += 2
             } else {
+                count += map[char]
                 i++
-                number += map[char]
             }
         } else {
+            count += map[char];
             i++
-            number += map[char]
         }
     }
-    return number
-}
-console.log(decoreded("LV"))
+    return count
+ }

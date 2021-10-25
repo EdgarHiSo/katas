b99c480d95b08163a962d338fabfa3e521a0b0dc
Edgar HiSo <edghiso@gmail.com>
2021-10-25 11:40:00 +0200

    feat: making practice

--- ironhack/traya.js
+++ ironhack/traya.js
@@ -1,24 +1,10 @@
-function decoderNumber(roman) {
-    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
-    let chars = roman.split("")
-    let number = 0;
-    let i = 0;
-    while (i < chars.length) {
-        let char = chars[i]
-        if (i < chars.length - 1) {
-            let next = chars[i + 1]
-            if (char + next in map) {
-                number += map[char + next]
-                i += 2
-            } else {
-                number += map[char]
-                i++
-            }
-        } else {
-            number += map[char]
-            i++
+function isPrim(number) {
+    for (let i = 2; i < number; i++) {
+        if (number % 2 === 0) {
+            return false
         }
     }
-    return number
+    return number > 1
 }
-console.log(decoderNumber("LXXX"))
+
+console.log(isPrim(33))

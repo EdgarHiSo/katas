86bd025c22f19c7d0377e5feebd915961ccf3d65
Edgar HiSo <edghiso@gmail.com>
2021-10-25 11:48:18 +0200

    feat: making practice

--- ironhack/traya.js
+++ ironhack/traya.js
@@ -1,10 +1,14 @@
-function isPrim(number) {
-    for (let i = 2; i < number; i++) {
-        if (number % 2 === 0) {
-            return false
+let array = [1,2,4]
+function twoSum(array, target) {
+    let indixes = []
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === target) {
+                indixes.push(i)
+                indixes.push(j)
+            }
         }
     }
-    return number > 1
+    return indixes
 }
-
-console.log(isPrim(33))
+console.log(twoSum(array, 5))

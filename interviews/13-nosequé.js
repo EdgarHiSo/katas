449c7240cb315a84d3fa3875570ca20f566b3f2e
Edgar HiSo <edghiso@gmail.com>
2021-11-17 11:44:59 +0100

    counting letters

--- interviews/13-nosequé.js
+++ interviews/13-nosequé.js
@@ -3,6 +3,7 @@
   En este caso devolveria “a3b2c”
 [12:17, 12/11/2021] Nacho Hispano: Otro ejemplo: “aaaaffffddrrtt” 
 Devuelveme: “a4f4d2r2t2” */
+//Kata 2019: Combine Fruits
 
 let str = "aaaaffffddrrtt"
 let str1 = "aabbaa"
@@ -34,4 +35,31 @@ function getHow(str) {
    }
    return result.join("")
 }
-console.log(getHow(str1))
+
+
+function compress(s) {
+    let ans = ""
+    let i = 0
+    while (i < s.length) {
+        let letter = s[i]
+        let count = 1
+        while (letter == s[i + 1]) {
+            count++
+            i++
+        }
+        ans += letter
+        if (count > 1) {
+            ans += count
+        }
+        i++
+    }
+    return ans
+}
+
+let ss = "aaaaffffddrrtta"
+console.log(compress(ss))
+
+
+
+
+

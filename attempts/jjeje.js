e6f3a23a259c4bd9a3f78dd34a41307c775fe18a
Edgar HiSo <edghiso@gmail.com>
2021-12-06 13:34:19 +0100

    some practicing

--- attempts/jjeje.js
+++ attempts/jjeje.js
@@ -1,6 +1,6 @@
 function validParens(parens) {
     let map = { ")": "(", "}": "{", "]": "[" }
-    let stack = [];
+    let stack = []
     for (let paren of parens) {
         if (!map[paren]) {
             stack.push(paren)
@@ -10,10 +10,10 @@ function validParens(parens) {
     }
     return stack.length == 0
 }
-
 function encoder(number) {
-    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
-    let output = ""
+
+    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    let output = "";
     for (let i in map) {
         while (number >= map[i]) {
             output += i
@@ -24,7 +24,7 @@ function encoder(number) {
 }
 
 function decoder(roman) {
-    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
     let chars = roman.split("")
     let count = 0
     let i = 0
@@ -46,54 +46,39 @@ function decoder(roman) {
     }
     return count
 }
+
 function isPrim(number) {
-   if (number <= 1) return false
+    if (number <= 1) return false
     for (let i = 2; i <= Math.sqrt(number); i++) {
         if (number % i === 0) {
             return false
         }
-    }
-   return true
+    }    
+    return true
 }
 
 function twoSum(array, target) {
+    let indexes = []
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[i] + array[j] === target) {
-                return [i, j]
+                indexes.push(i, j)
             }
         }
     }
+    return indexes
 }
-
-function sortStr(str) {
+ 
+function sortWord(str) {
     return str.split("").sort().join("")
 }
 function isAnagram(str1, str2) {
-    return sortStr(str1) === sortStr(str2)
-}
-function groupAnagrams(array) {
-    const lists = []
-    const representatives = []
-    for (let i = 0; i < array.length; i++) {
-        const representative = sortStr(array[i])
-        if (!representatives.includes(representative)) {
-            const list = [array[i]]
-            for (let j = i + 1; j < array.length; j++) {
-                if (isAnagram(array[i], array[j])) {
-                    list.push(array[j])
-                }
-            }
-            lists.push(list)
-            representatives.push(representative)
-        }
-    }
-    return lists
+    return sortWord(str1) === sortWord(str2)
 }
 
-function countAnagrams(array) {
-     let count = 0
-    for (let i = 0; i < array.length; i++) {
+function countAnagram(array) {
+    let count = 0;
+    for (let i = 0; i < array.length - 1; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (isAnagram(array[i], array[j])) {
                 count++
@@ -102,50 +87,6 @@ function countAnagrams(array) {
     }
     return count
 }
-
-function scramblies(str1, str2) {
-    let mapLetters = {};
-
-    str2.split("").forEach(letter => {
-        if (letter in mapLetters) {
-            mapLetters[letter]++
-        } else {
-            mapLetters[letter] = 1
-        }
-    })
-    str1.split("").forEach(letter => {
-        if (letter in mapLetters) {
-            mapLetters[letter]--
-        }
-    })
-    return Object.values(mapLetters).every(letter => letter <= 0)
-}
-
-function getClosest(array, target) {
-    return array.reduce((prev, curr) => (target - curr) < (target - prev) && curr < target ? curr: prev, Number.MIN_VALUE)
-}
-
-function getCoins(cents) {
-    let mapCoin = { 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0 };
-    do {
-        M = getClosest(Object.keys(mapCoin), cents)
-        mapCoin[M]++
-        cents -= M
-    } while( cents != 0)
-    return mapCoin
-}
-
-function flattenArray(array) {
-    let newArr = []
-    for (let subarray of array) {
-        for (let elm of subarray) {
-            newArr.push(elm)
-        }
-    }
-    return newArr
-}
-function sortNames(array) {
-    let newArr = flattenArray(array)
-
-   
+function groupAnagram(array) {
+    
 }
--- interviews/fibonacci.js
+++ interviews/fibonacci.js
@@ -3,3 +3,15 @@ const fib = function (n) {
     if (n === 1) return 1
     return fib(n - 1) + fib(n - 2)
   }
+
+
+function fastFib(n) {
+  var arr = [0, 1]
+  for (let i = 0; i < n; i++) {
+    arr = [arr[1], arr[0] + arr[1]]
+  }
+  return arr[0]
+}
+console.log(fastFib(9))
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34
